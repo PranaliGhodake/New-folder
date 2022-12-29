f120470e06b6cf3337799bdd2296776a66c413e9
Pranali G <pranalighodake22@gmail.com>
2022-12-29 23:44:07 +0530

    commit1

---
+++ Pranali/proto.js
@@ -0,0 +1,37 @@
+/*let a = {
+    name: "harry",
+    language: "java"
+}
+console.log(a)
+let p = {
+    run: () => {
+        alert("run")
+    }
+}
+
+a.__proto__ = p
+a.run()*/
+
+
+class RailwayForm {
+    submit() {
+        alert(this.name + ":your form is submitted")
+    }
+    cancel() {
+        alert(this.name + ":this form is cancelled")
+    }
+    fill(givenname) {
+        this.name = givenname
+    }
+}
+
+let harryForm = new RailwayForm()
+harryForm.fill("harry")
+
+let rohanForm = new RailwayForm()
+rohanForm.fill("rohan")
+
+
+rohanForm.submit()
+harryForm.submit()
+rohanForm.cancel()
